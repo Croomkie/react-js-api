61ae829da71c3765e4f078640d9a1e5197665339
LuluDmn <ldp030602@gmail.com>
2025-02-13 21:15:59 +0100

    fzefezfefezfze

--- src/bdd.js
+++ src/bdd.js
@@ -9,5 +9,7 @@ dotenv.config();
  */
 export const sequelize = new Sequelize({
 	dialect: MySqlDialect,
-	url: 'mysql://root:oTjIMohQCUFHLvPpCHLVxYLQaTkyUTAB@roundhouse.proxy.rlwy.net:26965/railway',
+	url: process.env.MYSQL_ADDON_URI || 'mysql://root:oTjIMohQCUFHLvPpCHLVxYLQaTkyUTAB@roundhouse.proxy.rlwy.net:26965/railway',
 });
+
+//mysql://uijjlicdxqnssr2a:UN1ZVTiZ0VgDYW6hQCjJ@br1fanb5glatx6ktotzm-mysql.services.clever-cloud.com:3306/br1fanb5glatx6ktotzm
